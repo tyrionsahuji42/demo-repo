ff2d99fe4d0deb093a3b6094cce286b8c1ee9d33
tanishsahu42 <tanishsahu43@gmail.com>
2024-01-02 10:12:50 +0530

    my first commit

---
+++ routes/web.js
@@ -0,0 +1,39 @@
+
+const authController = require('../app/http/controllers/authController')
+const homeController=require('../app/http/controllers/homeController')
+const cartController=require('../app/http/controllers/customer/cartController')
+const orderController=require('../app/http/controllers/customer/orderController')
+const adminOrderController = require('../app/http/controllers/admin/orderController')
+const statusController = require('../app/http/controllers/admin/statusController')
+
+
+const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
+const admin = require('../app/http/middleware/admin')
+
+
+function initRoutes(app)
+{
+        
+    app.get('/',  homeController().index )
+    app.get('/cart', cartController().index)
+
+    app.get('/login',guest,authController().login)
+    app.post('/login',authController().postLogin)
+
+    app.get('/register',guest,authController().register)
+    app.post('/register',authController().postRegister)
+    app.post('/logout', authController().logout)
+
+    app.post('/update-cart',cartController().update)
+    app.post('/orders', auth, orderController().store)
+    app.get('/customer/orders',auth,orderController().index)
+    app.get('/customer/orders/:id',auth,orderController().show)
+
+
+    app.get('/admin/orders',admin,  adminOrderController().index)
+    app.post('/admin/order/status', admin, statusController().update)
+}
+
+
+module.exports=initRoutes;
